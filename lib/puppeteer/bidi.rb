7df73f7d971d84bda00dc71ee017427455f85d48
YusukeIwaki <iwaki@i3-systems.com>
2025-12-13 20:44:16 +0900

    Make Puppeteer::BiDi.launch to accept block with browser

--- lib/puppeteer/bidi.rb
+++ lib/puppeteer/bidi.rb
@@ -33,20 +33,40 @@ require "puppeteer/bidi/browser"
 module Puppeteer
   module Bidi
     # Launch a new browser instance
+    #
     # @rbs executable_path: String? -- Path to browser executable
     # @rbs user_data_dir: String? -- Path to user data directory
-    # @rbs headless: bool -- Run browser in headless mode
+    # @rbs headless: bool? -- Run browser in headless mode
     # @rbs args: Array[String]? -- Additional browser arguments
     # @rbs timeout: Numeric? -- Launch timeout in seconds
-    # @rbs return: Browser -- Browser instance
-    def self.launch(executable_path: nil, user_data_dir: nil, headless: true, args: nil, timeout: nil)
-      Browser.launch(
-        executable_path: executable_path,
-        user_data_dir: user_data_dir,
-        headless: headless,
-        args: args,
-        timeout: timeout
-      )
+    #
+    #: (executable_path: String? , user_data_dir: String? , headless: bool , args: Array[String]? , timeout: Numeric?) -> Browser
+    #: (executable_path: String? , user_data_dir: String? , headless: bool , args: Array[String]? , timeout: Numeric?) { (Browser) -> untyped } -> untyped
+    def self.launch(executable_path: nil, user_data_dir: nil, headless: true, args: nil, timeout: nil, &block)
+      if block
+        Sync do
+          begin
+            browser = Browser.launch(
+              executable_path: executable_path,
+              user_data_dir: user_data_dir,
+              headless: headless,
+              args: args,
+              timeout: timeout
+            )
+            block.call(browser)
+          ensure
+            browser&.close
+          end
+        end
+      else
+        Browser.launch(
+          executable_path: executable_path,
+          user_data_dir: user_data_dir,
+          headless: headless,
+          args: args,
+          timeout: timeout
+        )
+      end
     end
 
     # Connect to an existing browser instance
