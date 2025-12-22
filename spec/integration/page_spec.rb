56a5c2abed1c7ab660c62e9c0b63f0a694ecbccd
YusukeIwaki <iwaki@i3-systems.com>
2025-12-22 23:32:59 +0900

    Implement Page.pdf and its test

--- spec/integration/page_spec.rb
+++ spec/integration/page_spec.rb
@@ -1231,8 +1231,6 @@ RSpec.describe 'Page' do
 
   describe 'Page.pdf' do
     it 'should generate a pdf' do
-      pending 'Page.pdf not implemented'
-
       with_test_state do |page:, server:, **|
         page.goto("#{server.prefix}/grid.html")
         pdf = page.pdf
@@ -1243,8 +1241,6 @@ RSpec.describe 'Page' do
     end
 
     it 'should generate a pdf and save to file' do
-      pending 'Page.pdf not implemented'
-
       with_test_state do |page:, server:, **|
         Dir.mktmpdir do |dir|
           output_path = File.join(dir, 'output.pdf')
