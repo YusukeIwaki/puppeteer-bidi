fd265e7e8c0694853ae0c8261e9e69c7b393fc39
YusukeIwaki <iwaki@i3-systems.com>
2025-12-31 23:26:38 +0900

    Add usecase test with Minitest

---
+++ test/usecase/example_github_screenshot_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+require 'tmpdir'
+require 'chunky_png'
+
+class ExampleGithubScreenshotTest < Minitest::Test
+  def test_take_screenshot_using_block
+    Puppeteer::Bidi.launch(headless: false) do |browser|
+      page = browser.new_page
+      page.goto('https://github.com/YusukeIwaki')
+
+      Dir.mktmpdir do |dir|
+        screenshot_path = File.join(dir, 'screenshot.png')
+        page.screenshot(path: screenshot_path)
+
+        png = ChunkyPNG::Image.from_file(screenshot_path)
+        assert png.width > 0
+        assert png.height > 0
+      end
+    end
+  end
+
+  def test_take_screenshot_using_instance
+    browser = Puppeteer::Bidi.launch_browser_instance(headless: false)
+
+    # works even outside of Sync block and separate thread!
+    thread = Thread.new do
+      begin
+        page = browser.new_page
+        page.goto('https://github.com/YusukeIwaki')
+        Dir.mktmpdir do |dir|
+          screenshot_path = File.join(dir, 'screenshot.png')
+          page.screenshot(path: screenshot_path)
+
+          png = ChunkyPNG::Image.from_file(screenshot_path)
+          assert png.width > 0
+          assert png.height > 0
+        end
+      ensure
+        browser.close
+      end
+    end
+
+    thread.join
+  end
+end
